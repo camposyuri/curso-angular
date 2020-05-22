f6a53d5bfe1099183d8a3918c2570ad7ca1fe44b
camposyuri <yurinapoleao@gmail.com>
2020-05-22 10:10:25 -0300

    [ADD] - ViewChild

--- comminication-app/src/app/parent-child/parent-child.component.ts
+++ comminication-app/src/app/parent-child/parent-child.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { TimerComponent } from './timer/timer.component';
 
 @Component({
   selector: 'app-parent-child',
   templateUrl: './parent-child.component.html',
-  styleUrls: ['./parent-child.component.css']
+  styleUrls: ['./parent-child.component.css'],
 })
 export class ParentChildComponent implements OnInit {
+  @ViewChild(TimerComponent)
+  private mytimer: TimerComponent;
 
-  constructor() { }
+  @ViewChild('myP')
+  private myp: ElementRef;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  start() {
+    this.mytimer.start();
   }
 
+  stop() {
+    this.mytimer.stop();
+  }
+
+  clear() {
+    this.mytimer.clear();
+  }
+
+  ngAfterViewInit() {
+    console.log(this.myp);
+  }
 }
